1d163d8f9776a0c6cd9aa05837fb300e5336b685
KimDongJin <dj8793@gmail.com>
2023-11-15 07:35:32 +0900

    fix : 입력 재실행 책임, View -> Controller 변경

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,5 +1,7 @@
 package christmas.controller;
 
+import christmas.domain.Order;
+import christmas.domain.Planner;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
@@ -14,6 +16,30 @@ public class EventPlannerController {
     }
 
     public void run() {
-        inputView.printGreeting();
+        int date;
+        Order order;
+
+        while (true) {
+            try {
+                date = inputView.printGreeting();
+                break;
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                order = new Order(inputView.printMenuSelect());
+                break;
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        outputView.printOrderList(order);
+        outputView.printOrderTotalPrice(order);
+
+        Planner plan = new Planner(date, order);
     }
 }
