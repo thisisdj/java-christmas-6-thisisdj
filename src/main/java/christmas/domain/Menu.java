5fd47739f9886de8b574f2cdac1951cbd37c69ca
KimDongJin <dj8793@gmail.com>
2023-11-15 07:33:55 +0900

    feat : MenuType Enum을 통한 메뉴타입 관리

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -2,31 +2,31 @@ package christmas.domain;
 
 public enum Menu {
     // 에피타이저
-    MushroomSoup("양송이수프", 6_000, "에피타이저"),
-    Tapas("타파스", 5_500, "에피타이저"),
-    CaesarSalad("시저샐러드", 8_000, "에피타이저"),
+    MushroomSoup("양송이수프", 6_000, MenuType.APPETIZER),
+    Tapas("타파스", 5_500, MenuType.APPETIZER),
+    CaesarSalad("시저샐러드", 8_000, MenuType.APPETIZER),
 
     // 메인
-    T_BornStake("티본스테이크", 55_000, "메인"),
-    BBQRib("바비큐립", 54_000, "메인"),
-    SeafoodPasta("해산물파스타", 35_000, "메인"),
-    XMasPasta("크리스마스파스타", 25_000, "메인"),
+    T_BornStake("티본스테이크", 55_000, MenuType.MAIN),
+    BBQRib("바비큐립", 54_000, MenuType.MAIN),
+    SeafoodPasta("해산물파스타", 35_000, MenuType.MAIN),
+    XMasPasta("크리스마스파스타", 25_000, MenuType.MAIN),
 
     // 디저트
-    ChocoCake("초코케이크", 15_000, "디저트"),
-    IceCream("아이스크림", 5_000, "디저트"),
+    ChocoCake("초코케이크", 15_000, MenuType.DESSERT),
+    IceCream("아이스크림", 5_000, MenuType.DESSERT),
 
     // 음료
-    ZeroCola("제로콜라", 3_000, "음료"),
-    RedWine("레드와인", 60_000, "음료"),
-    Champagne("샴페인", 25_000, "음료"),
+    ZeroCola("제로콜라", 3_000, MenuType.DRINK),
+    RedWine("레드와인", 60_000, MenuType.DRINK),
+    Champagne("샴페인", 25_000, MenuType.DRINK),
     ;
 
     private final String name;
     private final int price;
-    private final String type;
+    private final MenuType type;
 
-    Menu(String name, int price, String type) {
+    Menu(String name, int price, MenuType type) {
         this.name = name;
         this.price = price;
         this.type = type;
@@ -40,7 +40,7 @@ public enum Menu {
         return price;
     }
 
-    public String getType() {
+    public MenuType getType() {
         return type;
     }
 }
